19373001c83096017ec64ed646ed8ba8afbcd7d2
Austin Marcus <bwvxnt@gmail.com>
2018-04-08 02:25:58 -0400

    befor merge

--- src/screens/CreateEventScreen.js
+++ src/screens/CreateEventScreen.js
@@ -6,12 +6,59 @@ import { StackNavigator } from 'react-navigation';
 import { NavigationActions } from 'react-navigation';
 
 class CreateEventScreen extends React.Component{
-  static navigationOptions = {title: "Create New Event..."}
+  static navigationOptions = {title: "Create New Event"}
+
+  constructor(props){
+    super(props)
+
+    this.state = ({
+      date: '',
+      time: '',
+      title: '',
+      location: '',
+      info: '',
+    })
+  }
+
+  createNewEventInDB = () => {
+    this.setState({
+      info: this.state.date + this.state.time
+    })
+  }
 
   render(){
     return(
+      <View style = {{flex:1 , flexDirection: "column", justifyContent: "flex-start", padding: 10}}>
+        // date entry
+        <Item floatingLabel>
+          <Label>Date</Label>
+          <Input onChangeText={(date)=>this.setState({date})}/>
+        </Item>
 
-      <Text>"Hello World"</Text>
+        <Item floatingLabel>
+          <Label>Time</Label>
+          <Input onChangeText={(time)=>this.setState({time})}/>
+        </Item>
+
+        <Item floatingLabel>
+          <Label>Title</Label>
+          <Input />
+        </Item>
+
+        <Item floatingLabel>
+          <Label>Location</Label>
+          <Input />
+        </Item>
+
+        <Button
+          title="Create..."
+          onPress={()=> this.createNewEventInDB()}
+        />
+
+        <Text>{this.state.info}</Text>
+
+      </View>
+
 
     )
 
